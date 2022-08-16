9ea35f07ff2817f5c30dd0bbdef14a26cc1ade26
Tomas Kliner <tomasklinerr@gmail.com>
2022-08-16 15:48:23 +0200

    save before State management

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,33 +1,40 @@
 import React from "react";
+import Meme from '../memesData'
 
 export default function Header() {
-  function handleClick() {}
-  function handleMouseEnter() {
-   // document.getElementsByName('img').src='https://picsum.photos/640/36';
+    let url;
+  function handleClick() {
+    if (Meme.success){
+        const memesData= Meme.data.memes[Math.floor(Math.random() * Meme.data.memes.length)]; 
+        url = memesData.url;
+        alert(url)
+
+    }
   }
+
   return (
     <div>
-      <form className="w-1/3 m-auto flex flex-col justify-between">
-        <div className="flex justify-between my-4">
+      <div className="w-1/3 m-auto flex flex-col justify-between">
+        <div className="flex justify-between my-4 w-auto">
           <input
             type="text"
-            className="border-gray-300 border-2 rounded-md p-1"
-            value="Shut up"
+            className="border-gray-300 border-2 rounded-md p-1 w-auto"
+            placeholder="Shut up"
           />
           <input
             type="text"
-            className="border-gray-300 border-2 rounded-md p-1"
-            value="And take my money"
+            className="border-gray-300 border-2 rounded-md p-1 w-auto"
+            placeholder="And take my money"
           />
         </div>
         <input
           type="submit"
           value="Get a new meme image 🖼️"
           onClick={handleClick}
-          className="bg-gradient-to-r from-purple-800 to-purple-500 text-white p-2 w-full rounded-md"
+          className="bg-gradient-to-r from-purple-800 to-purple-500 text-white p-2 w-full rounded-md cursor-pointer"
         />
-      </form>
-      <img className="m-auto mt-4" onMouseEnter={handleMouseEnter} src="https://picsum.photos/640/360" />
+      </div>
+      <img className="m-auto mt-4" src={url} alt="meme" />
     </div>
   );
 }
