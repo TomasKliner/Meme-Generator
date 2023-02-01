11a4407ac07f6348ae79693ba3ad7a7a2d3ac0e5
Tomas Kliner <tomasklinerr@gmail.com>
2023-02-01 12:15:05 +0100

    woking meme generator

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,14 @@ import troll from "../troll.svg";
 
 export default function Header() {
   return (
-    <header className="bg-gradient-to-r from-purple-800 to-purple-500 text-white flex justify-between p-4">
+    <header className="bg-gradient-to-r from-red-500 to-orange-500 text-white flex justify-between p-4">
       <div className="flex justify-center">
         <img src={troll} alt="troll face logo"></img>
         <h1 className="text-xl px-2 font-bold">Meme generator</h1>
       </div>
-      <p className="font-light">React Course - Project 3</p>
+      <a href="kliner.eu">
+        <p className="font-light hover:border-b-2">kliner.eu</p>
+      </a>
     </header>
   );
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,23 +1,19 @@
-import React from "react"
-import memesData from "../memesData.js"
+import React from "react";
+import memesData from "../memesData.js";
 
 export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "Shut up",
+    bottomText: "And take my money",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const allMemeImages = memesData;
 
-    
-    const [meme, setMeme] = React.useState({
-        topText:"",
-        bottomText:"",
-        randomImage: "http://i.imgflip.com/1bij.jpg"
-    });
-    const allMemeImages = memesData;
-    
-    
-    function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMeme({...meme, randomImage: memesArray[randomNumber].url})
-        
-    }
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMeme({ ...meme, randomImage: memesArray[randomNumber].url });
+  }
 
   return (
     <div>
@@ -27,21 +23,35 @@ export default function Meme() {
             type="text"
             className="border-gray-300 border-2 rounded-md p-1 w-auto"
             placeholder="Shut up"
+            onChange={(e) => {
+              setMeme({ ...meme, topText: e.target.value });
+            }}
           />
           <input
             type="text"
             className="border-gray-300 border-2 rounded-md p-1 w-auto"
             placeholder="And take my money"
+            onChange={(e) => {
+              setMeme({ ...meme, bottomText: e.target.value });
+            }}
           />
         </div>
         <input
           type="submit"
           value="Get a new meme image 🖼️"
           onClick={getMemeImage}
-          className="bg-gradient-to-r from-purple-800 to-purple-500 text-white p-2 rounded-md cursor-pointer w-full"
+          className="bg-gradient-to-r from-red-500 to-orange-500 text-white p-2 rounded-md cursor-pointer w-full"
         />
       </div>
-      <img className="m-auto mt-4 w-1/3" src={meme.randomImage} alt="meme " />
+      <div className="m-auto mt-4 w-1/3 relative text-white">
+        <img className="w-full" src={meme.randomImage} alt="meme " />
+        <div className="absolute top-0 w-full flex justify-center items-center">
+          <p className="font-bold text-3xl shadow-black drop-shadow-xl text-center" >{meme.topText}</p>
+        </div>
+        <div className=" flex items-center justify-center absolute bottom-0  w-full">
+          <p className="font-bold text-3xl shadow-black drop-shadow-md text-center">{meme.bottomText}</p>
+        </div>
+      </div>
     </div>
   );
 }
